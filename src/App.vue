bacb94cfaefed0b414f5ea852b47215ac5558519
Cristopher Zaiz <e.zaizortega@gmail.com>
2020-10-22 10:12:34 -0400

    starting project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import './assets/tailwind.css'
+
+export default {
+  name: 'app',
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+
+</style>
